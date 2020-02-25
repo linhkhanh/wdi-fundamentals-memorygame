f36ef475ee81f2bad644342f8456ccf90486a8e7
Linh Le <linhle@Hieus-MacBook-Air.local>
2020-02-25 14:22:06 +0800

    create game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "Queen";
+let cardTwo = "Queen";
+let CardThree = "King";
+let cardFour = "King";
+console.log("User flipped " + cardOne)
+console.log("User flipped " + CardThree)
